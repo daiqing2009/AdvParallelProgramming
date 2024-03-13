1538ae20e892d897d0517876c7137428eb87348d
戴青 <qing.dai@baozun.com>
2024-03-12 22:40:39 -0400

    initial print serial matrix

--- assignment2/question2/sum_element_serial.c
+++ assignment2/question2/sum_element_serial.c
@@ -1,27 +1,69 @@
 #include <stdio.h>
 #include <omp.h>
+#include <stdbool.h>
 
-#define SIZE 5
+#define PRT_PRE 3
+#define PRT_POST 2
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
 
-double dot_product(double *a, double *b, int size)
-{
-    double result = 0.0;
-
-#pragma omp parallel for reduction(+ : result)
-    for (int i = 0; i < size; ++i)
-    {
-        result += a[i] * b[i];
-    }
-
-    return result;
-}
+void print_matrix(char *prompt, int *mat, int m, int n);
 
 int main()
 {
-    double vec1[SIZE] = {9.0, 8.0, 7.0, 6.0, 5.0};
-    double vec2[SIZE] = {4.0, 5.0, 2.0, 9.0, 7.0};
+    int n, m, ip, jp;
+    int *a, *b;
+    printf("Please enter array dimention(m * n): \n");
+    scanf("%d %d", &m, &n);
 
-    double result = dot_product(vec1, vec2, SIZE);
-    printf("Dot product: %.2f\n", result);
+    /* initialize array A */
+    a = malloc(m * n * sizeof(int));
+    for (ip = 0; ip < m; ip++)
+    {
+        for (jp = 0; jp < n; jp++)
+        {
+            a[ip * m + jp] = ip * m + jp;
+        }
+    }
+
+    print_matrix("Matrix A:", a, m, n);
+    /* contrauct B */
+    b = malloc(m * n * sizeof(int));
+    for (ip = 0; ip < m; ip++)
+    {
+        for (jp = 0; jp < n; jp++)
+        {
+            if (jp == 0)
+            {
+                b[ip * m + jp] = b[(ip - 1) * m + jp] + a[ip * m + jp];
+            }
+            else if (ip == 0)
+            {
+                b[ip * m + jp] = b[ip * m + jp - 1] + a[ip * m + jp];
+            }
+            else
+            {
+                b[ip * m + jp] = b[(ip - 1) * m + jp] + b[ip * m + jp - 1] + a[ip * m + jp];
+            }
+        }
+    }
+
+    print_matrix("Matrix B:", b, m, n);
+
     return 0;
+}
+
+void print_matrix(char *prompt, int *mat, int m, int n)
+{
+    int i, j;
+    printf("%s\n", prompt);
+    // only print left-most portion of matrix when too large
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d\t", mat[i * m + j]);
+        }
+        printf("\n");
+    }
 }
