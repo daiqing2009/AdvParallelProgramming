e92c43dc02aaab9260f845e3b0a0f8938aeac8f1
戴青 <qing.dai@baozun.com>
2024-03-29 21:43:16 -0400

    first runnable version of code

--- assignment3/question1/min_dist.cu
+++ assignment3/question1/min_dist.cu
@@ -14,28 +14,33 @@
 #include <thrust/reduce.h>
 #include <thrust/execution_policy.h>
 
-// Number of times to run the test (for better timings accuracy):
-#define NTESTS 10
-
 // Number of threads in one block (possible range is 32...1024):
 #define BLOCK_SIZE 256
 
 // Total number of threads (total number of elements to process in the kernel):
-#define NMAX 512
+#define INIT_NMAX 4096
 
-#define NBLOCKS NMAX *(NMAX - 1) / BLOCK_SIZE / 2
+// Number of times to run the test (for scaling of dataset):
+#define NTESTS 3
+
+// Maximum value of
+#define MAX_DIST 1.41
 
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
-#define SQUARE(x) ((x) * (x))
+#define SQUARE(X) ((X) * (X))
 
+#define NMAX INIT_NMAX * 2
 // Input array (global host memory):
+// float *h_X;
+// float *h_Y;
+// float *h_dist;
 float h_X[NMAX];
 float h_Y[NMAX];
-float h_dist[NMAX * (NMAX - 1) / 2];
+float h_dist[NMAX * (NMAX - 1)];
 
 __device__ float d_X[NMAX];
 __device__ float d_Y[NMAX];
-__device__ float d_dist[NMAX * (NMAX - 1) / 2];
+// __device__ float *d_dist;
 __device__ float d_min;
 
 // It messes up with y!
@@ -58,7 +63,7 @@ __device__ static float fatomicMin(float *addr, float value)
 
 __global__ void init_kernel()
 {
-    d_min = 1.0;
+    d_min = MAX_DIST;
     return;
 }
 
@@ -69,17 +74,17 @@ __global__ void OneThreadPerParticleKernel()
     __shared__ float b_min[BLOCK_SIZE];
 
     int i = threadIdx.x + blockDim.x * blockIdx.x;
-    float thread_min, next_dist = 1;
+    float thread_min, next_dist = MAX_DIST;
 
     // Not needed, because NMAX is a power of two:
     // if (i >= NMAX)
     //     return;
-    
+
     // calculate the distance related to i and find the min of current thread
     for (int j = i + 1; j < BLOCK_SIZE; j += 2)
     {
         next_dist = sqrt(SQUARE(d_X[i] - d_X[j]) + SQUARE(d_Y[i] - d_Y[j]));
-        thread_min = next_dist < thread_min ? next_dist : thread_min;
+        thread_min = (next_dist < thread_min) ? next_dist : thread_min;
     }
 
     // To make sure all threads finished calc
@@ -115,18 +120,31 @@ __global__ void OneThreadPerPairKernel()
 
 int main(int argc, char **argv)
 {
-    struct timeval tdr0, tdr1, tdr;
-    double restime, min0;
+    struct timeval tdr0, tdr1, tdr2, tdr3;
+    double cputime, k1time, k2time;
+    double min0;
     float min;
     int error;
-
-    // cudaMalloc((void **)&d_X, NMAX * sizeof(float));
-    // cudaMalloc((void **)&d_Y, NMAX * sizeof(float));
-    // cudaMalloc((void **)&d_dist, NMAX * (NMAX - 1) / 2 * sizeof(float));
+    // int NMAX;
+    int NBLOCKS;
 
     // Loop to run the timing test multiple times:
     for (int kk = 0; kk < NTESTS; kk++)
     {
+        // double NMAX every time
+        // NMAX = INIT_NMAX * (int)pow(2, kk);
+        // printf("NMAX = %d", NMAX);
+
+        NBLOCKS = NMAX * (NMAX - 1) / BLOCK_SIZE;
+
+        // allocate CPU memory
+        // h_X = (float *)malloc(NMAX * sizeof(float));
+        // h_Y = (float *)malloc(NMAX * sizeof(float));
+
+        // allocate GPU memory
+        // cudaMalloc((void **)&d_X, NMAX * sizeof(float));
+        // cudaMalloc((void **)&d_Y, NMAX * sizeof(float));
+        // cudaMalloc((void **)&d_dist, NMAX * (NMAX - 1) * sizeof(float));
 
         // We don't initialize randoms, because we want to compare different strategies:
         // Initializing random number generator:
@@ -140,23 +158,32 @@ int main(int argc, char **argv)
         }
 
         // Computer distances in a CPU serial function
-        for (int i = NMAX - 1; i > 0; i--)
-            for (int j = 0; j < NMAX; j++)
+        gettimeofday(&tdr0, NULL);
+
+        for (int i = 0; i > NMAX - 1; i++)
+        {
+            // only need to calculate the upper half of the diagnal matrix
+            for (int j = 0; j < i; j++)
+                h_dist[i * NMAX + j] = MAX_DIST;
+            for (int j = i; j < NMAX; j++)
                 h_dist[i * NMAX + j] = sqrt(SQUARE(h_X[i] - h_X[j]) + SQUARE(h_Y[i] - h_Y[j]));
+        }
 
         // Find the minimal in serial way
-        min0 = 1;
+        min0 = MAX_DIST;
         for (int i = 0; i < (NMAX - 1) * NMAX; i++)
             if (h_dist[i] < min0)
                 min0 = (double)h_dist[i];
 
+        gettimeofday(&tdr1, NULL);
+
         // Copying the data to device (we don't time it):
-        if (error = cudaMemcpy(d_X, h_X, NMAX * sizeof(float), cudaMemcpyHostToDevice))
+        if (error = cudaMemcpyToSymbol(d_X, h_X, NMAX * sizeof(float), 0, cudaMemcpyHostToDevice))
         {
             printf("Error copy X to device %d\n", error);
             exit(error);
         }
-        if (error = cudaMemcpy(d_Y, h_Y, NMAX * sizeof(float), cudaMemcpyHostToDevice))
+        if (error = cudaMemcpyToSymbol(d_Y, h_Y, NMAX * sizeof(float), 0, cudaMemcpyHostToDevice))
         {
             printf("Error copy Y to device %d\n", error);
             exit(error);
@@ -169,17 +196,15 @@ int main(int argc, char **argv)
             exit(error);
         }
         //--------------------------------------------------------------------------------
-        gettimeofday(&tdr0, NULL);
+        gettimeofday(&tdr2, NULL);
 
         // Hybrid binary/atomic reduction:
         OneThreadPerParticleKernel<<<NBLOCKS, BLOCK_SIZE>>>();
 
-        // thrust::device_ptr<float> d_ptr_A(d_A);
-        // float reduction_sum = thrust::reduce(d_ptr_A, d_ptr_A + NMAX);
+        gettimeofday(&tdr3, NULL);
+        // tdr = tdr0;
+        // timeval_subtract(&restime, &tdr1, &tdr);
 
-        gettimeofday(&tdr1, NULL);
-        tdr = tdr0;
-        timeval_subtract(&restime, &tdr1, &tdr);
         // Copying the result back to host (we time it):
         if (error = cudaMemcpyFromSymbol(&min, d_min, sizeof(float), 0, cudaMemcpyDeviceToHost))
         {
@@ -192,16 +217,27 @@ int main(int argc, char **argv)
             printf("Error %d\n", error);
             exit(error);
         }
+
         printf("Min: %e (relative error %e)\n", min, fabs((double)min - min0) / min0);
 
-        printf("Time: %e\n", restime);
+        timeval_subtract(&cputime, &tdr1, &tdr0);
+        printf("CPU Time: %e\n", cputime);
+
+        timeval_subtract(&k1time, &tdr3, &tdr2);
+        printf("GPU kernel1(OneThreadPerParticle) Time: %e\n", k1time);
+        // printf("GPU kernel1(OneThreadPerParticle) Time: %e\n", k2time);
+
         //--------------------------------------------------------------------------------
+        // free GPU memory
+        // cudaFree(d_X);
+        // cudaFree(d_Y);
+        // cudaFree(d_dist);
 
+        // free CPU memory
+        // free(h_X);
+        // free(h_Y);
+        // free(h_Y);
     } // kk loop
-
-    // cudaFree(d_X);
-    // cudaFree(d_Y);
-    // cudaFree(d_dist);
 
     return 0;
 }
